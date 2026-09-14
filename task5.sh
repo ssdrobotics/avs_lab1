a3a9ddf6c70439221c868cd0c9fd01c71428a0e8
Sergey Solovev <sergio5748@mail.ru>
2026-09-14 18:40:48 +0000

    5 задание

---
+++ task5.sh
@@ -0,0 +1,19 @@
+cd lab0
+
+rm claude_monet/office/call_copy
+
+rm stock_link
+
+rm claude_monet/kitchen/warehouse_access
+
+rm claude_monet/meat_delivery/invoice_duplicate
+
+rm claude_monet/office/urgent_fish_order
+
+rmdir claude_monet/kitchen/cold_station
+
+rm claude_monet/warehouse/rejection_log
+
+rm -rf claude_monet/warehouse/fish_backup
+
+echo 'done'
